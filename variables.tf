aadb53f9dea0802d0cfb0e2bbc169b3da45587d1
Roystan Veigas <96535186+Roystanveigas@users.noreply.github.com>
2026-02-11 17:47:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,10 +15,11 @@ variable "key_name" {
 
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for remote state"
-  default = "tfdemo16112023"
+  default = "nemointergartionwithgit-terra"
 }
 
 variable "name" {
   description = "Name of EC2 Intance"
 
 }
+
